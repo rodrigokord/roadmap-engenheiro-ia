4867644dcd9c480c7a7f654f5255ebd728dc8d50
Rodrigo Kord <94815130+rokorde@users.noreply.github.com>
2026-09-18 10:10:09 -0300

    feat: cria pagina inicial do equilibrivm com estilo zen e fundo cascata

--- etapa-02-estruturas-dados/palword.py
+++ etapa-02-estruturas-dados/palword.py
@@ -1,18 +1,9 @@
 def contar(capturas):
     placar = 0
 
-    # TODO: Crie um loop 'for' para tirar cada 'pal' da lista 'capturas'
     for pal in capturas:
-    
-        # TODO: Crie um 'if' para verificar se o 'pal' atual é igual (==) a "Cattiva"
         if pal == "Cattiva":
             placar = placar + 1
-            
-        
-            # TODO: Se for verdade, pegue o 'placar' e some 1 a ele
-        else:
-            placar + 1
-            
 
     return placar
 
